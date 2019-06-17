a4e9156564700f87bd9d43f141849fdf4291624f
姬乙亥 <18537160262@qq.com>
2019-06-17 18:48:46 +0800

    1

---
+++ python/Django/20190614/test01/urllibs3.py
@@ -0,0 +1,18 @@
+from urllib import request
+from urllib.parse import urlencode
+
+
+kw = input('关键词>>>')
+
+data = {'q': kw}
+data = urlencode(data)
+
+# 像浏览器一样对网址发起请求，响应返回给了 result, 如果第二个参数存在则是post请求
+result = request.urlopen('https://huaban.com/search/?'+data)
+# result = request.urlopen('https://huaban.com/search/?q=卡哇伊')
+print(result.getcode())
+print(result.read().decode('utf-8'))
+
+
+
+
